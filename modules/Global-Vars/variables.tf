e2ecf0081858200985a6a530ea648afd161d394e
jayeshbanait <109805702+jayeshbanait@users.noreply.github.com>
2022-07-25 14:18:26 +0530

    Create variables.tf

---
+++ modules/Global-Vars/variables.tf
@@ -0,0 +1,13 @@
+variable "environment" {
+    default = "test"
+  description = ""
+}
+
+variable "region" {
+  #  default = "us-east-1"
+  description = "location of network"
+}
+variable "account_id" {
+  default = ""
+  
+}
