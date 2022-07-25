ce9164711a259342d9d6d25a3ce4ed4584e295e5
jayeshbanait <109805702+jayeshbanait@users.noreply.github.com>
2022-07-25 23:16:30 +0530

    Update backend.tf

--- domain/backend.tf
+++ domain/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
    encrypt = true
-   bucket  = "${var.environment}-${var.app_name}-env-s3"
+   bucket  = "test-helloworld-env-s3"
    key = "Statefile/terraform.tfstate"
    region  = "us-east-1"
   }
